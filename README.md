d502334675aa60386bc278db273a8b2c1bb7ff3e
Ben Hall <rcmast3r1@gmail.com>
2024-08-22 12:26:47 -0400

    updating readme and adding new packages to the devshell

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # LiDAR-Bike
+
+- `nix develop`
+
+- `ros2 run usb_cam usb_cam_node_exe --ros-args -p video_device:="/dev/video2" -p "pixel_format:=mjpeg2rgb" -p frame_rate:=60`
+
+- `ros2 launch foxglove_bridge foxglove_bridge_launch.xml`
--- test_nix_ros_overlay/flake.nix
+++ test_nix_ros_overlay/flake.nix
@@ -1,4 +1,9 @@
 {
+  nixConfig = {
+    extra-substituters = [ "https://ros.cachix.org" ];
+    extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
+  };
+
   inputs = {
     nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
     nixpkgs.follows = "nix-ros-overlay/nixpkgs";  # IMPORTANT!!!
@@ -6,6 +11,8 @@
   outputs = { self, nix-ros-overlay, nixpkgs }:
     nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
       let
+
+        
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ nix-ros-overlay.overlays.default ];
@@ -19,13 +26,19 @@
             (with pkgs.rosPackages.humble; buildEnv {
                 paths = [
                     ros-core
+                    ros-base
+                    foxglove-bridge
+                    rosbag2-storage-mcap
+                    (usb-cam.overrideAttrs (finalAttrs: previousAttrs: {
+                      propagatedBuildInputs = with pkgs; [ builtin-interfaces camera-info-manager cv-bridge ffmpeg_4 image-transport image-transport-plugins rclcpp rclcpp-components rosidl-default-runtime sensor-msgs std-msgs std-srvs v4l-utils ];
+                      nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ pkgs.pkg-config ];
+                    }))
                 ];
             })
           ];
         };
+
+        legacyPackages = pkgs;
       });
-  nixConfig = {
-    extra-substituters = [ "https://ros.cachix.org" ];
-    extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
-  };
+  
 }
