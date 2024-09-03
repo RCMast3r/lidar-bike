d4e26b8376621a29ff25522a0ea2b00f15c8380b
Ben Hall <rcmast3r1@gmail.com>
2024-09-03 12:11:49 -0400

    pushing up ouster ros addition to the environment with proper dds settings that allow for full rate of driver

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # LiDAR-Bike
 
+within `test_nix_ros_overlay`:
+
 - `nix develop`
 
 - `ros2 run usb_cam usb_cam_node_exe --ros-args -p video_device:="/dev/video2" -p "pixel_format:=mjpeg2rgb" -p frame_rate:=60`
 
 - `ros2 launch foxglove_bridge foxglove_bridge_launch.xml`
+
+- `ros2 launch ouster_ros sensor.launch.xml sensor_hostname:=169.254.87.155 viz:=false lidar_mode:=512x20`
--- test_nix_ros_overlay/flake.nix
+++ test_nix_ros_overlay/flake.nix
@@ -5,30 +5,43 @@
   };
 
   inputs = {
-    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/develop";
     nixpkgs.follows = "nix-ros-overlay/nixpkgs";  # IMPORTANT!!!
   };
-  outputs = { self, nix-ros-overlay, nixpkgs }:
+  outputs = { self, nix-ros-overlay, nixpkgs, simple-ouster-driver-src}:
     nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
       let
 
-        
+        # my_overlay = final: prev: {
+        #   simple-ouster-driver = final.callPackage ./simple_ouster_driver.nix { src = simple-ouster-driver-src; };
+        # };
+
+        # my-ros-overlay = final: prev: {
+        #   rosPackages = prev.rosPackages // { jazzy = prev.rosPackages.jazzy.overrideScope my_overlay; };
+        # };
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ nix-ros-overlay.overlays.default ];
+          overlays = [ nix-ros-overlay.overlays.default my_overlay ];
         };
       in {
         devShells.default = pkgs.mkShell {
-          name = "Example project";
+          name = "lidar-bike-env";
+          RMW_IMPLEMENTATION = "rmw_cyclonedds_cpp";
+          ROS_AUTOMATIC_DISCOVERY_RANGE="LOCALHOST";
+          RMW_CONNEXT_PUBLICATION_MODE="ASYNCHRONOUS";
+          CYCLONEDDS_URI="file://config/ddsconfig.xml";
           packages = [
             pkgs.colcon
+            
             # ... other non-ROS packages
-            (with pkgs.rosPackages.humble; buildEnv {
+            (with pkgs.rosPackages.jazzy; buildEnv {
                 paths = [
                     ros-core
                     ros-base
                     foxglove-bridge
                     rosbag2-storage-mcap
+                    ouster-ros
+                    rmw-cyclonedds-cpp
                     (usb-cam.overrideAttrs (finalAttrs: previousAttrs: {
                       propagatedBuildInputs = with pkgs; [ builtin-interfaces camera-info-manager cv-bridge ffmpeg_4 image-transport image-transport-plugins rclcpp rclcpp-components rosidl-default-runtime sensor-msgs std-msgs std-srvs v4l-utils ];
                       nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ pkgs.pkg-config ];
@@ -38,7 +51,7 @@
           ];
         };
 
-        legacyPackages = pkgs;
+        packages = pkgs;
       });
   
 }
